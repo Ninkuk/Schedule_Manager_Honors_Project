11a956de23685e479c73ef8a4b8132f15a40c2ca
Ninad Kulkarni <ninadk03@gmaik.com>
2019-11-23 11:12:59 -0700

    Created class and added constructor + getters/setters

--- src/data/Course.java
+++ src/data/Course.java
@@ -1,4 +1,45 @@
 package data;
 
-public class Course {
+import java.io.Serializable;
+
+public class Course implements Serializable {
+    private String title;
+    private String classCode;
+    private int days;
+    private String start;
+    private String end;
+    private int units;
+
+    public Course(String title, String classCode, int days, String start, String end, int units) {
+        this.title = title;
+        this.classCode = classCode;
+        this.days = days;
+        this.start = start;
+        this.end = end;
+        this.units = units;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getClassCode() {
+        return classCode;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public int getUnits() {
+        return units;
+    }
 }
