5847135e8923dec60cbdd2fe4a1069d070876fdb
Ninad Kulkarni <ninadk03@gmaik.com>
2019-11-23 11:22:58 -0700

    The main now fetches both student and course on start

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,9 @@
 import data.HandleData;
-import data.Student;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.NotSerializableException;
-import java.io.ObjectOutputStream;
 
 public class Main extends Application {
 
@@ -23,7 +17,8 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        HandleData.getList();
+        HandleData.getStudentList();
+        HandleData.getCourseList();
         launch(args);
     }
 }
