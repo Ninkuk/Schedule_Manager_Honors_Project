79847b87e93dd81d31ed44682513ccb0d387e8ff
Ninad Kulkarni <ninadk03@gmail.com>
2019-11-26 23:56:03 -0700

    Implemented "Add Course" button and error/success label

--- src/Controllers/ClassSearchController.java
+++ src/Controllers/ClassSearchController.java
@@ -2,12 +2,17 @@ package Controllers;
 
 import data.Course;
 import data.HandleData;
+import data.Student;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -16,11 +21,46 @@ import java.util.ResourceBundle;
 public class ClassSearchController implements Initializable {
     public TableView<Course> classSearchTable;
     public TableColumn<Course, String> titleColumn;
-    public TableColumn<Course, String> classCodeColumn;
+    public TableColumn<Course, Integer> classCodeColumn;
     public TableColumn<Course, String> daysColumn;
     public TableColumn<Course, String> startTimeColumn;
     public TableColumn<Course, String> endTimeColumn;
     public TableColumn<Course, Integer> unitsColumn;
+
+    public Button addCourseButton;
+    public Label errorMessageLabel;
+
+    public void addCourseToStudent() {
+        Course courseSelected = classSearchTable.getSelectionModel().getSelectedItem();
+
+        if (courseSelected == null) {
+            errorMessageLabel.setText("Please select a course first");
+            errorMessageLabel.setTextFill(Color.web("#f44336"));
+            errorMessageLabel.setVisible(true);
+        } else if (courseAlreadyExist(courseSelected)) {
+            errorMessageLabel.setText("Course already added to schedule");
+            errorMessageLabel.setTextFill(Color.web("#f44336"));
+            errorMessageLabel.setVisible(true);
+        } else {
+            HandleData.currentStudent.addCourse(courseSelected);
+            HandleData.saveStudentChanges();
+
+            //TODO: Add timer to remove message after a second
+
+            errorMessageLabel.setText("Course Added Successfully!");
+            errorMessageLabel.setTextFill(Color.web("#00bfa5"));
+            errorMessageLabel.setVisible(true);
+        }
+    }
+
+    private boolean courseAlreadyExist(Course courseSelected) {
+        for (Course course : HandleData.currentStudent.getCourses()) {
+            if (courseSelected.getClassCode() == course.getClassCode()) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
     @Override
@@ -31,17 +71,6 @@ public class ClassSearchController implements Initializable {
         startTimeColumn.setCellValueFactory(new PropertyValueFactory<>("start"));
         endTimeColumn.setCellValueFactory(new PropertyValueFactory<>("end"));
         unitsColumn.setCellValueFactory(new PropertyValueFactory<>("units"));
-        classSearchTable.setItems(getCourses());
-//        classSearchTable.getColumns().addAll(titleColumn, classCodeColumn, daysColumn, startTimeColumn, endTimeColumn, unitsColumn);
-    }
-
-    public ObservableList<Course> getCourses() {
-        ObservableList<Course> courses = FXCollections.observableArrayList(HandleData.coursesList);
-//        courses.add(new Course("bookTitle1", "230", 2, "today", "tomorrow", 5));
-//        courses.add(new Course("bookTitle2", "230", 2, "today", "tomorrow", 5));
-//        courses.add(new Course("bookTitle3", "230", 2, "today", "tomorrow", 5));
-//        courses.add(new Course("bookTitle4", "230", 2, "today", "tomorrow", 5));
-
-        return courses;
+        classSearchTable.setItems(FXCollections.observableArrayList(HandleData.coursesList));
     }
 }
