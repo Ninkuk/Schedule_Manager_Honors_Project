8ef6d16559d1ad42fb51ec7c452b3da03ad95e77
Ninad Kulkarni <ninadk03@gmail.com>
2019-11-27 00:02:07 -0700

    Constructor now only accepts title and unit hours. Days and Times are randomly generated as LocalTime type.

--- src/data/Course.java
+++ src/data/Course.java
@@ -1,30 +1,74 @@
 package data;
 
 import java.io.Serializable;
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Course implements Serializable {
     private String title;
-    private String classCode;
+    private int classCode;
     private ArrayList<String> days;
-    private String start;
-    private String end;
+    private LocalTime start;
+    private LocalTime end;
     private int units;
 
-    public Course(String title, String classCode, ArrayList<String> days, String start, String end, int units) {
+    public Course(String title, int units) {
         this.title = title;
-        this.classCode = classCode;
-        this.days = days;
-        this.start = start;
-        this.end = end;
+        this.classCode = setClassCode();
+
+        this.days = new ArrayList<>();
+
+        if (units == 3) {
+            this.days.add("M");
+            this.days.add("W");
+            this.days.add("F");
+        } else if (units == 2) {
+            this.days.add("T");
+            this.days.add("Th");
+        } else {
+            this.days.add("F");
+        }
+
+        setStartEndTime(units);
         this.units = units;
+    }
+
+    private int setClassCode() {
+        Random random = new Random();
+        int classCodeRandom = random.nextInt(9000) + 1000;
+        for (int i = 0; i < HandleData.coursesList.size(); i++) {
+            if (HandleData.coursesList.get(i).getClassCode() == classCodeRandom) {
+                i = 0;
+                classCodeRandom = random.nextInt(10000);
+            }
+        }
+
+        return classCodeRandom;
+    }
+
+    private void setStartEndTime(int units) {
+        Random random = new Random();
+        boolean bool = random.nextBoolean();
+        LocalTime time = LocalTime.NOON;
+        LocalTime startTime;
+        if (bool) {
+            startTime = time.plus(Duration.ofMinutes(random.nextInt(60) * 5));
+            this.start = startTime;
+            this.end = startTime.plus(Duration.ofHours(units));
+        } else {
+            startTime = time.minus(Duration.ofMinutes(random.nextInt(80) * 5));
+            this.start = startTime;
+            this.end = startTime.plus(Duration.ofHours(units));
+        }
     }
 
     public String getTitle() {
         return title;
     }
 
-    public String getClassCode() {
+    public int getClassCode() {
         return classCode;
     }
 
@@ -36,11 +80,11 @@ public class Course implements Serializable {
         return daysList;
     }
 
-    public String getStart() {
+    public LocalTime getStart() {
         return start;
     }
 
-    public String getEnd() {
+    public LocalTime getEnd() {
         return end;
     }
 
